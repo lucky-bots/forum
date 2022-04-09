631e7ceabb8c71d9bcb53438b7167599a8ef4fe7
lucky-botsecho <kunsakol@hotmail.com>
2022-04-09 18:58:14 +0700

    [commit message]

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '56edfd06e21d7050947db4b4949c28010cdb01ed',
+        'name' => 'u0_a227/forum',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'u0_a227/forum' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '56edfd06e21d7050947db4b4949c28010cdb01ed',
+            'dev_requirement' => false,
+        ),
+    ),
+);
