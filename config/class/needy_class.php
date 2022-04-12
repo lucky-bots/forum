1c9e100eb11e9872b3e51a2ddf116855cfdeb715
lucky-botsecho <kunsakol@hotmail.com>
2022-04-12 21:47:46 +0700

    [commit message]

---
+++ config/class/needy_class.php
@@ -0,0 +1,24 @@
+<?php
+
+  class N{
+
+    public static $e;
+    public static $database;
+    public static $DIR = "/insta";
+    public static $GMAIL = "jjhu488@gmail.com";
+    public static $GMAIL_PASSWORD = "Aakk0000";
+
+    public static function _DB(){
+      try {
+        self::$database = new PDO('mysql:host=sql6.freesqldatabase.com;dbname=sql6484665;charset=utf8mb4','sql6484665','HEa8wmPhy8');
+        self::$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $e = self::$e;
+      } catch (PDOException $e) {
+        echo $e->getMessage();
+      }
+      return self::$database;
+    }
+
+  }
+
+?>
